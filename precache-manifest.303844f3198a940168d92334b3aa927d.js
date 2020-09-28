33b660e67dee46f69f34ddba135a50d72b359ea6
Neha Bharati <nehabharati@Nehas-MacBook-Pro.local>
2020-09-28 17:39:37 +0530

    Updates

---
+++ precache-manifest.303844f3198a940168d92334b3aa927d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36dd86538b31083ea3306649032284e6",
+    "url": "/video-grid/index.html"
+  },
+  {
+    "revision": "a61a4ab6ea3a44aa2d9f",
+    "url": "/video-grid/static/css/main.93bd20c5.chunk.css"
+  },
+  {
+    "revision": "d4517e7b2643395aceb4",
+    "url": "/video-grid/static/js/2.64d3d600.chunk.js"
+  },
+  {
+    "revision": "0d1f70cf55389d2d2003fcca6dd48e62",
+    "url": "/video-grid/static/js/2.64d3d600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61a4ab6ea3a44aa2d9f",
+    "url": "/video-grid/static/js/main.3cd1533d.chunk.js"
+  },
+  {
+    "revision": "b37e5a9a663a4ebb6786",
+    "url": "/video-grid/static/js/runtime-main.e7d5370f.js"
+  },
+  {
+    "revision": "72aef8b05f5705e0a2bedef2653b1482",
+    "url": "/video-grid/static/media/happy.72aef8b0.svg"
+  },
+  {
+    "revision": "65f11b42fcd3bb6f7f92d344ff4c92f7",
+    "url": "/video-grid/static/media/hero.65f11b42.svg"
+  },
+  {
+    "revision": "cba123971a1fbc8a71c00c26a4d28502",
+    "url": "/video-grid/static/media/search.cba12397.svg"
+  },
+  {
+    "revision": "8b8f9c680cc641e09e923a456915e466",
+    "url": "/video-grid/static/media/stream.8b8f9c68.svg"
+  }
+]);
